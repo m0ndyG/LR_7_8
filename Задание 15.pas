6b4025386735da006b11493b0e3948b93f5bbbdf
m0ndyG <144677345+m0ndyG@users.noreply.github.com>
2023-11-29 17:50:06 +0300

    Update Задание 15.pas

--- Задание 15.pas
+++ Задание 15.pas
@@ -1,32 +1,10 @@
-﻿program z_15;
-var
-  a, b: string;
-  i: integer;
+var s: string;
+i: integer;
 begin
-  writeln('Введите строку:');
-  readln(a); // считывание строки
-  
-  b := ''; // создаем пустую строку
-  i := 1;
-  
-  while i <= length(a) do // пока i меньше или равно входной строке
-  begin
-    if (a[i] = 'x') and (i < length(a) - 2) and (a[i + 1] = 'a') and (a[i + 2] = 'b') and (a[i + 3] = 'c') then // нахождение символов
-    begin             // проверка, есть ли три символа
-
-      b := b + 'abc'; // добавляем abc к выходной строке
-      i := i + 4; // увеличиваем, что бы пропустить x, a, b, c
-    end
-    else
-    begin
-
-      b := b + a[i]; // добавление текущего символа к выходной строке
-      i := i + 1; // переход к след. символу во входной строке
-    end;
-  end;
-  
-  writeln('Результат:');
-  writeln(b);
+  readln(s);
+  for i:=length(s) downto 4 do
+    if (s[i-3] = 'x') and (copy(s, i-2, 3) = 'abc') then
+      delete(s, i-3, 1);
+  writeln (s);
 end.
-
 
